a21cb319f4882dee85fe2b2e9e53690f9f3986cf
Jonah Irizarry <jonah.irizarry@gmail.com>
2025-07-01 22:38:34 -0400

    added the first project card and also changed the logo a bit

--- website/src/main.rs
+++ website/src/main.rs
@@ -4,6 +4,8 @@ use dioxus::prelude::*;
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 // const HEADER_SVG: Asset = asset!("/assets/header.svg");
 const RESUME_PDF: Asset = asset!("/assets/Jonah_Irizarry_Resume.pdf");
+const FFXIV_WTUI: Asset = asset!("/assets/FFXIV_WTUI.png");
+const FFXIV_TUI_GIF: Asset = asset!("/assets/FFXIV_TUI.gif");
 
 fn main() {
     // web_sys::console::log_1(&"Starting...".into()); //console.info()
@@ -19,10 +21,18 @@ fn App() -> Element {
     // }
     rsx! {
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        div{
+        nav{
             // class: if,
             id: "header",
-            a{href: "#", "Jonah Irizarry"}
+            span{
+                id: "logo",
+                pre{
+                    "   ▗▖ ▗▄▖ ▗▖  ▗▖ ▗▄▖ ▗▖ ▗▖    ▗▄▄▄▖▗▄▄▖ ▗▄▄▄▖▗▄▄▄▄▖ ▗▄▖ ▗▄▄▖ ▗▄▄▖▗▖  ▗▖
+   ▐▌▐▌ ▐▌▐▛▚▖▐▌▐▌ ▐▌▐▌ ▐▌      █  ▐▌ ▐▌  █     ▗▞▘▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▝▚▞▘ 
+   ▐▌▐▌ ▐▌▐▌ ▝▜▌▐▛▀▜▌▐▛▀▜▌      █  ▐▛▀▚▖  █   ▗▞▘  ▐▛▀▜▌▐▛▀▚▖▐▛▀▚▖ ▐▌  
+▗▄▄▞▘▝▚▄▞▘▐▌  ▐▌▐▌ ▐▌▐▌ ▐▌    ▗▄█▄▖▐▌ ▐▌▗▄█▄▖▐▙▄▄▄▖▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌ ▐▌  "
+                }
+            }
             div {
                 // id: "nav",
                 id: "links",
@@ -57,7 +67,7 @@ fn AbtMe() -> Element{
             id: "about",
             h1 {"About Me"},
             p {"I’m a passionate computer science student and AI enthusiast who recently completed my Bachelor’s in Computer Science with a focus on Artificial Intelligence through Binghamton University’s 4+1 program (expected Master’s completion in May 2026). My academic and project work, spanning AI-powered tools like a Tesseract OCR-integrated dataset processor, a Rust-based feed aggregator, and a Flask-driven web scraper, has deepened my fascination with leveraging AI to solve complex problems. I’m particularly drawn to local AI tools like Ollama and llama.cpp, which empower developers to deploy and refine models independently."},
-            p{"Beyond the classroom, I’ve applied my skills as a Solutions Engineer at Sense Education, where I explored integrating Chat-GPT 3.5 to streamline data processing and improve model adaptability. My work also includes leading a team to build an AI moderation tool for web scraping projects and developing terminal-based applications in C++ and Rust. I thrive at the intersection of machine learning, software development, and user-centric design, and I’m excited to continue pushing the boundaries of AI using both cutting-edge technologies and local hosting tools like Ollama to create impactful, real-world solutions."},
+            p{"Beyond the classroom, I’ve applied my skills as a Solutions Engineer at Sense Education, where I explored integrating Chat-GPT to streamline data processing and improve model adaptability. My work also includes leading a team to build an AI moderation tool for web scraping projects and developing terminal-based applications in C++ and Rust. I thrive at the intersection of machine learning, software development, and user-centric design, and I’m excited to continue pushing the boundaries of AI using both cutting-edge technologies and local hosting tools like Ollama to create impactful, real-world solutions."},
         },        
     }
 }
@@ -69,7 +79,19 @@ fn ProjectsPage() -> Element{
             id: "projects",
             class: "deactive",
             h1 {"Projects"},
-            "This is where projects will go. Most likely on some sort of carrosel or slider"
+            div{
+                class:"project-box",
+                img{
+                    class:"pimgs",
+                    src:"{FFXIV_WTUI}",
+                    "onmouseover": "this.src='{FFXIV_TUI_GIF}'",
+                    "onmouseout": "this.src='{FFXIV_WTUI}'",
+                    alt:"A screenshot of the Final Fantasy XIV Weather terminal application"
+                },
+                p{class:"pname","Final Fantasy XIV Weather App"},
+                p{class:"planguages","C++"},
+                p{class:"pdesc","A terminal app to display the weather in all areas of FFXIV."}
+            }
         },
     }
 }
@@ -87,7 +109,7 @@ fn ResumePage() -> Element{
                 src: "{RESUME_PDF}",
                 // src: "/run/user/1000/doc/f60e0210/Jonah_Irizarry_Resume.pdf",
                 r#type: "application/pdf",
-                width: "1000",
+                width: "900",
                 height: "1200",
             },
         },
